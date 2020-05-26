1eb6c8eaa1f101ebaf1c545b431bbf6149fb0239
norving <65973025+norving@users.noreply.github.com>
2020-05-26 13:06:34 -0500

    Add files via upload

---
+++ files/Exp-Menus.js
@@ -0,0 +1,18 @@
+/*$(document).ready(function() {
+  
+    $(".logo-lupa").hover(
+      // Cuando entra el mouse
+      function() {
+        console.log("abrir menu slide")
+        $(".menu-lupa").show()
+      },
+      // cuando sale el mouse
+      function() {
+        console.log("cerrar menu slide")
+        $(".menu-lupa").hide()
+      })
+
+    
+      
+    
+  }) */
