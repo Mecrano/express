1874558d7939c13b572f7290ce6d05d6db259bf1
Daigoro Jimenez <daigo.09j@gmail.com>
2020-05-26 13:16:19 -0500

    Configuracion inicial de git

---
+++ files/EXP-General.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+  $(".logo-lupa").hover(
+      // Cuando entra el mouse
+      function () {
+          console.log("abrir menu slide");
+          $(".menu-lupa").show();
+      },
+      // cuando sale el mouse
+      function () {
+          console.log("cerrar menu slide");
+          $(".menu-lupa").hide();
+      }
+  );
+  $(".logo-mi-cuenta").hover(
+      // Cuando entra el mouse
+      function () {
+          console.log("abrir menu slide");
+          $(".menu-mi-cuenta").show();
+      },
+      // cuando sale el mouse
+      function () {
+          console.log("cerrar menu slide");
+          $(".menu-mi-cuenta").hide();
+      }
+  );
+  $(".logo-carrito").hover(
+      // Cuando entra el mouse
+      function () {
+          console.log("abrir menu slide");
+          $(".menu-carrito").show();
+    },
+      // cuando sale el mouse
+      function () {
+          console.log("cerrar menu slide");
+          $(".menu-carrito").hide();
+    }
+);
+});
