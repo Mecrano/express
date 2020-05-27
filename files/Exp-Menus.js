e09bc6c44d61c3e78e3c7df018f68a7a62c4f786
Norving <norving.cedeno@sihold.com>
2020-05-27 08:03:14 -0500

    Header Actualizado

--- files/Exp-Menus.js
+++ files/Exp-Menus.js
@@ -1,18 +1,79 @@
-/*$(document).ready(function() {
+$(document).ready(function() {
   
     $(".logo-lupa").hover(
       // Cuando entra el mouse
       function() {
         console.log("abrir menu slide")
-        $(".menu-lupa").show()
+        document.getElementById("menu-slide-lupa").style.width = "30%";
       },
       // cuando sale el mouse
       function() {
         console.log("cerrar menu slide")
-        $(".menu-lupa").hide()
+        document.getElementById("menu-slide-lupa").style.width = "0%";
       })
 
+      $(".menu-lupa").hover(
+        // Cuando entra el mouse
+        function() {
+          console.log("abrir menu slide")
+          document.getElementById("menu-slide-lupa").style.width = "30%";
+        },
+        // cuando sale el mouse
+        function() {
+          console.log("cerrar menu slide")
+          document.getElementById("menu-slide-lupa").style.width = "0%";
+        })
+
+
+      $(".logo-mi-cuenta").hover(
+        // Cuando entra el mouse
+        function() {
+          console.log("abrir menu slide")
+          document.getElementById("menu-slide-cuenta").style.width = "30%";
+        },
+        // cuando sale el mouse
+        function() {
+          console.log("cerrar menu slide")
+          document.getElementById("menu-slide-cuenta").style.width = "0%";
+        })
+  
+
+        $(".menu-mi-cuenta").hover(
+          // Cuando entra el mouse
+          function() {
+            console.log("abrir menu slide")
+            document.getElementById("menu-slide-cuenta").style.width = "30%";
+          },
+          // cuando sale el mouse
+          function() {
+            console.log("cerrar menu slide")
+            document.getElementById("menu-slide-cuenta").style.width = "0%";
+          })
+  
+        
+        
+        $(".logo-carrito").hover(
+          // Cuando entra el mouse
+          function() {
+            console.log("abrir menu slide")
+            document.getElementById("menu-slide-carrito").style.width = "30%";
+          },
+          // cuando sale el mouse
+          function() {
+            console.log("cerrar menu slide")
+            document.getElementById("menu-slide-carrito").style.width = "0%";
+          })
+          $(".menu-carrito").hover(
+            // Cuando entra el mouse
+            function() {
+              console.log("abrir menu slide")
+              document.getElementById("menu-slide-carrito").style.width = "30%";
+            },
+            // cuando sale el mouse
+            function() {
+              console.log("cerrar menu slide")
+              document.getElementById("menu-slide-carrito").style.width = "0%";
+            })
     
-      
-    
-  }) */
+  
+  }) 
