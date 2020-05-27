7054eca5718cbddc8f2f13010afae7a12bf0c096
Edwin Alexander Obando <edwin.obando@itglobers.com>
2020-05-27 13:33:29 -0500

    Actualización de Codigo de Norving

--- files/Exp-Menus.js
+++ files/Exp-Menus.js
@@ -1,91 +1,107 @@
-$(document).ready(function() {
-  
-  $(".logo-lupa").hover(
-    // Cuando entra el mouse
-    function() {
-      console.log("abrir menu slide")
-      document.getElementById("menu-slide-lupa").style.width = "30%";
-      document.getElementById("background-opaco").style.visibility = "visible";
-    },
-    // cuando sale el mouse
-    function() {
-      console.log("cerrar menu slide")
-      document.getElementById("menu-slide-lupa").style.width = "0%";
-      document.getElementById("background-opaco").style.visibility = "hidden";
-    })
+$(document).ready(function () {
+    $(".logo-lupa").hover(
+        // Cuando entra el mouse
+        function () {
+            console.log("abrir menu slide");
+            $("#menu-slide-lupa").css("width", "30%");
+            $("#background-opaco").addClass("active");
+        },
+        // cuando sale el mouse
+        function () {
+            console.log("cerrar menu slide");
+            $("#menu-slide-lupa").css("width", "0%");
+            $("#background-opaco").removeClass("active");
+        }
+    );
 
     $(".menu-lupa").hover(
-      // Cuando entra el mouse
-      function() {
-        console.log("abrir menu slide")
-        document.getElementById("menu-slide-lupa").style.width = "30%";
-        document.getElementById("background-opaco").style.visibility = "visible";
-      },
-      // cuando sale el mouse
-      function() {
-        console.log("cerrar menu slide")
-        document.getElementById("menu-slide-lupa").style.width = "0%";
-        document.getElementById("background-opaco").style.visibility = "hidden";
-      })
-
+        // Cuando entra el mouse
+        function () {
+            console.log("abrir menu slide");
+            $("#menu-slide-lupa").css("width", "30%");
+            $("#background-opaco").addClass("active");
+        },
+        // cuando sale el mouse
+        function () {
+            console.log("cerrar menu slide");
+            $("#menu-slide-lupa").css("width", "0%");
+            $("#background-opaco").removeClass("active");
+        }
+    );
 
     $(".logo-mi-cuenta").hover(
-      // Cuando entra el mouse
-      function() {
-        console.log("abrir menu slide")
-        document.getElementById("menu-slide-cuenta").style.width = "30%";
-        document.getElementById("background-opaco").style.visibility = "visible";
-      },
-      // cuando sale el mouse
-      function() {
-        console.log("cerrar menu slide")
-        document.getElementById("menu-slide-cuenta").style.width = "0%";
-        document.getElementById("background-opaco").style.visibility = "hidden";
-      })
-
-
-      $(".menu-mi-cuenta").hover(
         // Cuando entra el mouse
-        function() {
-          console.log("abrir menu slide")
-          document.getElementById("menu-slide-cuenta").style.width = "30%";
-          document.getElementById("background-opaco").style.visibility = "visible";
+        function () {
+            console.log("abrir menu slide");
+            $("#menu-slide-cuenta").css("width", "30%");
+            $("#background-opaco").addClass("active");
         },
         // cuando sale el mouse
-        function() {
-          console.log("cerrar menu slide")
-          document.getElementById("menu-slide-cuenta").style.width = "0%";
-          document.getElementById("background-opaco").style.visibility = "hidden";
-        })
+        function () {
+            console.log("cerrar menu slide");
+            $("#menu-slide-cuenta").css("width", "0%");
+            $("#background-opaco").removeClass("active");
+        }
+    );
 
-      
-      
-      $(".logo-carrito").hover(
+    $(".menu-mi-cuenta").hover(
         // Cuando entra el mouse
-        function() {
-          console.log("abrir menu slide")
-          document.getElementById("menu-slide-carrito").style.width = "30%";
-          document.getElementById("background-opaco").style.visibility = "visible";
+        function () {
+            console.log("abrir menu slide");
+            $("#menu-slide-cuenta").css("width", "30%");
+            $("#background-opaco").addClass("active");
         },
         // cuando sale el mouse
-        function() {
-          console.log("cerrar menu slide")
-          document.getElementById("menu-slide-carrito").style.width = "0%";
-          document.getElementById("background-opaco").style.visibility = "hidden";
-        })
-        $(".menu-carrito").hover(
-          // Cuando entra el mouse
-          function() {
-            console.log("abrir menu slide")
-            document.getElementById("menu-slide-carrito").style.width = "30%";
-            document.getElementById("background-opaco").style.visibility = "visible";
-          },
-          // cuando sale el mouse
-          function() {
-            console.log("cerrar menu slide")
-            document.getElementById("menu-slide-carrito").style.width = "0%";
-            document.getElementById("background-opaco").style.visibility = "hidden";
-          })
-  
+        function () {
+            console.log("cerrar menu slide");
+            $("#menu-slide-cuenta").css("width", "0%");
+            $("#background-opaco").removeClass("active");
+        }
+    );
 
-}) 
+    $(".logo-carrito").hover(
+        // Cuando entra el mouse
+        function () {
+            console.log("abrir menu slide");
+            $("#menu-slide-carrito").css("width", "30%");
+            $("#background-opaco").addClass("active");
+        },
+        // cuando sale el mouse
+        function () {
+            console.log("cerrar menu slide");
+            $("#menu-slide-carrito").css("width", "0%");
+            $("#background-opaco").removeClass("active");
+        }
+    );
+    $(".menu-carrito").hover(
+        // Cuando entra el mouse
+        function () {
+            console.log("abrir menu slide");
+            $("#menu-slide-carrito").css("width", "30%");
+            $("#background-opaco").addClass("active");
+        },
+        // cuando sale el mouse
+        function () {
+            console.log("cerrar menu slide");
+            $("#menu-slide-carrito").css("width", "0%");
+            $("#background-opaco").removeClass("active");
+        }
+    );
+
+    $(".nav.navbar .dropdown").hover(
+        // Cuando entra el mouse
+        function () {
+            $(".menu-inferior").css("z-index", "2");
+            $("#background-opaco").addClass("active");
+            $("#background-opaco").css("top", "13.2rem");
+            $(this).addClass("active");
+        },
+        // cuando sale el mouse
+        function () {
+            $("#background-opaco").removeClass("active");
+            $("#background-opaco").css("top", "6.8rem");
+            $(this).removeClass("active");
+            $(".menu-inferior").css("z-index", "1");
+        }
+    );
+});
