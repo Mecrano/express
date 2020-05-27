08ad67dc4c2df8eacfba18f0679c58cc848493a0
Norving <norving.cedeno@sihold.com>
2020-05-27 12:22:24 -0500

    Cambio header

--- files/Exp-Menus.js
+++ files/Exp-Menus.js
@@ -5,6 +5,7 @@ $(document).ready(function() {
       function() {
         console.log("abrir menu slide")
         document.getElementById("menu-slide-lupa").style.width = "30%";
+        document.getElementById("background-opaco").style.visibility = "visible";
       },
       // cuando sale el mouse
       function() {
