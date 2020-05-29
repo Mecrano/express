2c9a3eae6da3fed77af0bc81451fbbf97f4ccea6
Alvaroquiroz14 <aquiroz@fastcoint.com>
2020-05-29 12:20:21 -0500

    UPDATE

---
+++ Exp-Menus.js
@@ -0,0 +1,120 @@
+$(document).ready(function () {
+    $(".logo-lupa").hover(
+        // Cuando entra el mouse
+        function () {
+            console.log("abrir menu slide");
+            $("#menu-slide-lupa").css("width", "30%");
+            $("#background-opaco").addClass("active");
+        },
+        // cuando sale el mouse
+        function () {
+            console.log("cerrar menu slide");
+            $("#menu-slide-lupa").css("width", "0%");
+            $("#background-opaco").removeClass("active");
+        }
+    );
+
+    $(".menu-lupa").hover(
+        // Cuando entra el mouse
+        function () {
+            console.log("abrir menu slide");
+            $("#menu-slide-lupa").css("width", "30%");
+            $("#background-opaco").addClass("active");
+        },
+        // cuando sale el mouse
+        function () {
+            console.log("cerrar menu slide");
+            $("#menu-slide-lupa").css("width", "0%");
+            $("#background-opaco").removeClass("active");
+        }
+    );
+
+    $(".logo-mi-cuenta").hover(
+        // Cuando entra el mouse
+        function () {
+            console.log("abrir menu slide");
+            $("#menu-slide-cuenta").css("width", "30%");
+            $("#background-opaco").addClass("active");
+        },
+        // cuando sale el mouse
+        function () {
+            console.log("cerrar menu slide");
+            $("#menu-slide-cuenta").css("width", "0%");
+            $("#background-opaco").removeClass("active");
+        }
+    );
+
+    $(".menu-mi-cuenta").hover(
+        // Cuando entra el mouse
+        function () {
+            console.log("abrir menu slide");
+            $("#menu-slide-cuenta").css("width", "30%");
+            $("#background-opaco").addClass("active");
+        },
+        // cuando sale el mouse
+        function () {
+            console.log("cerrar menu slide");
+            $("#menu-slide-cuenta").css("width", "0%");
+            $("#background-opaco").removeClass("active");
+        }
+    );
+
+    $(".logo-carrito").hover(
+        // Cuando entra el mouse
+        function () {
+            console.log("abrir menu slide");
+            $("#menu-slide-carrito").css("width", "30%");
+            $("#background-opaco").addClass("active");
+        },
+        // cuando sale el mouse
+        function () {
+            console.log("cerrar menu slide");
+            $("#menu-slide-carrito").css("width", "0%");
+            $("#background-opaco").removeClass("active");
+        }
+    );
+    $(".menu-carrito").hover(
+        // Cuando entra el mouse
+        function () {
+            console.log("abrir menu slide");
+            $("#menu-slide-carrito").css("width", "30%");
+            $("#background-opaco").addClass("active");
+        },
+        // cuando sale el mouse
+        function () {
+            console.log("cerrar menu slide");
+            $("#menu-slide-carrito").css("width", "0%");
+            $("#background-opaco").removeClass("active");
+        }
+    );
+
+    $(".nav.navbar .dropdown").hover(
+        // Cuando entra el mouse
+        function () {
+            $(".menu-inferior").css("z-index", "2");
+            $("#background-opaco").addClass("active");
+            $("#background-opaco").css("top", "13.2rem");
+            $(this).addClass("active");
+        },
+        // cuando sale el mouse
+        function () {
+            $("#background-opaco").removeClass("active");
+            $("#background-opaco").css("top", "6.8rem");
+            $(this).removeClass("active");
+            $(".menu-inferior").css("z-index", "1");
+        }
+    );
+    
+    $('#banners-principal').slick({
+        "autoplay": true,
+        dots: true,
+        infinite: true,
+        speed: 1500,
+        fade: true,
+        cssEase: 'linear'
+    });
+
+ 
+});
+
+
