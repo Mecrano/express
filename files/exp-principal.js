934084b43139cd2d6f76416b98f772525a9fe7a6
GaryBravo <gbravo@fastcoint.com>
2020-05-29 08:27:43 -0500

    Correccion del menu

--- files/exp-principal.js
+++ files/exp-principal.js
@@ -1,3 +1,33 @@
+const formatter = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+});
+
+function crearCarrito() {
+    vtexjs.checkout.getOrderForm().done(function (orderForm) {
+        $(".menu-carrito").append(
+            "<p>" +
+                "Subtotal: " +
+                formatter.format(orderForm.totalizers[0].value / 100) +
+                ", Total: " +
+                formatter.format(orderForm.value / 100) +
+            "</p>"
+        );
+        orderForm.items.forEach(function (item) {
+            $(".menu-carrito").append(
+                "<a href='" + item.detailUrl +"'>" +
+                    "<img src='" + item.imageUrl + "'>" +
+                    "<div>" +
+                        "Nombre: " + item.name + 
+                        ", Precio: " + formatter.format(item.sellingPrice / 100) +
+                        ", Cantidad: " + item.quantity +
+                    "</div>" +
+                "</a>"
+            );
+        });
+    });
+}
+
 $(document).ready(function () {
     $(".logo-lupa").hover(
         // Cuando entra el mouse
@@ -86,12 +116,12 @@ $(document).ready(function () {
             $("#menu-slide-carrito").css("width", "0%");
             $("#background-opaco").removeClass("active");
         }
-    ),
+    );
 
-    $(".nav .navbar .dropdown").hover(
+    $(".nav.navbar .dropdown").hover(
         // Cuando entra el mouse
         function () {
-            $(".menu-inferior").css("z-index", "2");
+            $(".menu_inferior").css("z-index", "2");
             $("#background-opaco").addClass("active");
             $("#background-opaco").css("top", "13.2rem");
             $(this).addClass("active");
@@ -101,8 +131,18 @@ $(document).ready(function () {
             $("#background-opaco").removeClass("active");
             $("#background-opaco").css("top", "6.8rem");
             $(this).removeClass("active");
-            $(".menu-inferior").css("z-index", "1");
+            $(".menu_inferior").css("z-index", "1");
         }
-    )
+    );
+
+
+    // Solo activar en ambiente VTEX
+    // crearCarrito()
+
+    //Activa el Banner
+    $('#banners-principal').slick({
+        "autoplay": true,
+        "autoplaySpeed": 1500
+    });
 }
 );
